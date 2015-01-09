b82545dcaa34534095fc49a5d96460854383adf8
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-01-09 17:23:58 +0100

    Fixed issue with multiple chrome windows.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,19 +1,28 @@
 'use strict';
 
+var console = chrome.extension.getBackgroundPage().console;
+
+
 register('google', 'http://webcache.googleusercontent.com/search?q=cache:'); 
 register('way-back', 'https://web.archive.org/web/*/');
 
 function register(id, prefix) {
   document.getElementById(id).addEventListener('click', function() {
-    chrome.tabs.query({'active': true} , function(tabs) { 
-      var tab = tabs[0];
+    chrome.windows.getCurrent(function(window) {
+      chrome.tabs.query({
+        active: true,
+        windowId: window.id
+      } , function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        var index = tab.index + 1;
+        chrome.tabs.create({
+          url: prefix + url,
+          index: index
+        });
 
-      var url = tab.url;
-      var index = tab.index + 1;
-      chrome.tabs.create({
-        url: prefix + url,
-        index: index
       });
+
     });
   });
 }
